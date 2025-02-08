239b47a880d47fa4c3946b11f8ba652da3eabbbd
LuisJavierSamaniegoRosales <22-10378@usb.ve>
2025-02-07 21:36:10 -0400

    C:\Users\lsama\OneDrive\Documentos\CI2125

---
+++ Laboratorio 1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    double num1 = 53;
+    double num2 = -27;
+    double num3 = 0.005;
+    double suma = num1 + num2 + num3;
+    std::cout << "La suma de los números 53, -27 y 0.005 es: " << suma << std::endl;
+
+    return 0;
+}
